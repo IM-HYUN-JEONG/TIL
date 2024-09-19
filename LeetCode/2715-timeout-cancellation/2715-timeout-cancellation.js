b802853ee8b037a4e360e4b6b3b08d5f0340263d
Hyun <95120267+IM-HYUN-JEONG@users.noreply.github.com>
2024-09-19 13:59:19 +0900

    Time: 75 ms (10.62%), Space: 49.2 MB (80.34%) - LeetHub

---
+++ LeetCode/2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,41 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    // 실행을 지연시키기 위해 setTimeout을 사용
+    const timeoutId = setTimeout(() => {
+        fn(...args); // 지연이 끝나면 함수 실행
+    }, t);
+
+    // 취소할 수 있는 cancelFn 반환
+    return function cancelFn() {
+        clearTimeout(timeoutId); // 지연된 실행을 취소
+    };
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
